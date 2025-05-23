6f878810fb19a835bfe0f4374cf5afcce7d2981c
AjayNaik01 <naikajay952@gmail.com>
2025-05-23 17:31:40 +0530

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -14,7 +14,7 @@ const app = express();
 // Middlewares
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173", // OR use "*" in prod
+    origin: "http://localhost:5173", 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
