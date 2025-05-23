e1335db66e880019314f3e2b68fdc8a1b90e6aa8
AjayNaik01 <naikajay952@gmail.com>
2025-05-23 16:26:45 +0530

    Update config.js

--- BACKEND/src/config/config.js
+++ BACKEND/src/config/config.js
@@ -1,6 +1,6 @@
 export const cookieOption = {
     httpOnly: true,
-    secure: false,
+    secure: true,
     sameSite: "Lax",
     maxAge: 1000 * 60 * 60 * 24, // 24 hour 0 minutes in milliseconds
     path: '/'
