26a3bbc913e0704ef1676cbecea51da9fe1ff6c1
AjayNaik01 <naikajay952@gmail.com>
2025-05-27 13:04:00 +0530

    Update user.api.js

--- FRONTEND/src/api/user.api.js
+++ FRONTEND/src/api/user.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Get the API base URL from environment variables
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+const API_BASE_URL =  'https://url-shortner-eight-lime.vercel.app';
 
 // Set default axios config
 axios.defaults.withCredentials = true;
